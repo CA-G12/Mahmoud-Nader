6f623281051d7da272d0c6942416fe42841e43b2
nader shakshak <nadershak149@gmail.com>
2022-09-22 08:37:51 +0300

    update the cards style

--- src/components/CardsComponents/index.js
+++ src/components/CardsComponents/index.js
@@ -4,18 +4,25 @@ class CardsComponents extends Component {
   render() {
     const { movie } = this.props;
     return (
-          <div className="card">
-            <img className="img-card" src={movie.large_cover_image} alt=""/>
-            <div className="details">
-              <p className="p-movie-name">{movie.title}</p>
-              <p className="p-movie-name"><span class="fa fa-star checked"></span>{movie.rating}</p>
-            </div>
-            <a className="button" href={movie.url} target="_blank" rel="noreferrer" >More</a>
-          </div>
+      // <div className="card">
+      //   <img className="img-card" src={movie.large_cover_image} alt=""/>
+      //   <div className="details">
+      //     <p className="p-movie-name">{movie.title}</p>
+      //     <p className="p-movie-name"><span class="fa fa-star checked"></span>{movie.rating}</p>
+      //   </div>
+      //   <a className="button" href={movie.url} target="_blank" rel="noreferrer" >More</a>
+      // </div>
+      <div className="imgDivAll">
+        <img className="imgDiv" src={movie.large_cover_image} alt = "" />
+        <div className="overlay">
+          <h3 className="h2Name">{movie.title}</h3>
+          <a href={movie.url} target="_blank" rel="noreferrer" >
+            See Video
+          </a>
+        </div>
+      </div>
     );
   }
 }
 
 export default CardsComponents;
-
-
